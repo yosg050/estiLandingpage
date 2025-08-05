696090ec0ecdfdb85dbc19f7db982b843310cf61
yosef <yosefglr@gmail.com>
2025-08-06 00:05:20 +0300

    Another attempt 8

--- next.config.mjs
+++ next.config.mjs
@@ -1,26 +1,16 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   // swcMinify: true,
-//   // eslint: {
-//   //   ignoreDuringBuilds: true,
-//   // },
-//   // typescript: {
-//   //   ignoreBuildErrors: true,
-//   // },
-//   images: {
-//     unoptimized: true,
-//   },
-// };
-
-// export default nextConfig
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  trailingSlash: true,
+  // swcMinify: true,
+  // eslint: {
+  //   ignoreDuringBuilds: true,
+  // },
+  // typescript: {
+  //   ignoreBuildErrors: true,
+  // },
   images: {
-    unoptimized: true
-  }
-}
+    unoptimized: true,
+  },
+};
 
 export default nextConfig
+
